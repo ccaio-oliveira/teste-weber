1c7b8f3c686fac662a5584fde948a586496ff8a0
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-07-25 22:20:44 -0300

    adicionando validador de email

--- config/valida_login.php
+++ config/valida_login.php
@@ -3,4 +3,29 @@
 session_start();
 require_once './conectar_db.php';
 
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+function validaEmail($email){
+
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if(strlen($senha) === 0) {
+
+    header('Location: ../index.php?senha=vazio');
+
+} else if(!validaEmail($email)){
+
+    header('Location: ../index.php?email=invalido');
+
+} //else {
+
+//     $sql_code = 'SELECT '
+
+// }
 ?>
--- index.php
+++ index.php
@@ -5,9 +5,19 @@
 
     <div id="container">
         <h1>Login</h1>
-        <form action="./config/valida_login.php">
+        <form action="./config/valida_login.php" method="POST">
             <input type="text" id="email" name="email" placeholder="E-mail">
+            <?php if(isset($_GET['email']) == 'invalido'){ ?>
+                <div class="error">
+                    <p>E-mail inválido</p>
+                </div>
+            <?php } ?>
             <input type="senha" id="senha" name="senha" placeholder="Senha">
+            <?php if(isset($_GET['senha']) == 'vazio'){ ?>
+                <div class="error">
+                    <p>Senha inválida</p>
+                </div>
+            <?php } ?>
             <button type="submit">Login</button>
         </form>
     </div>

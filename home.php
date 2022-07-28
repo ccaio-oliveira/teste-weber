9c08be9e89366a509c4379268c545fd003c613fd
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-07-28 16:41:31 -0300

    finalizando ajustes no projeto

--- home.php
+++ home.php
@@ -87,13 +87,45 @@ $filtro = filter_input(INPUT_GET, 'filterProd', FILTER_SANITIZE_STRING);
                 <h1>Produtos disponíveis</h1>
                 <div id="prod-list">
 
-                    <?php if (isset($_GET['produto']) == 'vazio') { ?>
+                    <?php if(!isset($_GET) == false){
+
+                        $cont = $sql_query_prod->num_rows;
+
+                        if($cont > 0){
+
+                            do { ?>
+
+                                <div class="prod-item">
+
+                                    <div><?php $imagem = base64_encode($produtos['foto']); echo "<img src='data:image/png;base64,$imagem'/>";?></div>
+                                    <div class="prod-text">
+                                        <h2><?= $produtos['nome_produto'] ?></h2>
+                                    </div>
+                                    <div class="prod-info">
+                                        <p class="preco">R$<?= $produtos['preco'] ?></p>
+                                        <p class="categ">Categoria: <?= $produtos['categoria'] ?></p>
+                                        <button class="remove-prod" onclick="apagarProduto(<?= $produtos['id_produto']; ?>)"><i class="fa-solid fa-trash-can"></i> Remover</button>
+                                    </div>
+
+                                </div>
+
+                            <?php } while ($produtos = $sql_query_prod->fetch_assoc());
+
+                        } else { ?>
+
+                            <div class="prod-vazio">
+                                <h2>Nenhum produto disponível</h2>
+                            </div>
+
+                        <?php }
+                        
+                    } else if (isset($_GET['produto']) == 'vazio') { ?>
 
                         <div class="prod-vazio">
                             <h2>Nenhum produto disponível</h2>
                         </div>
 
-                    <?php } if($_GET['filterProd'] == 'selected') {
+                    <?php } else if($_GET['filterProd'] == 'selected') {
 
                         do { ?>
 
@@ -118,25 +150,37 @@ $filtro = filter_input(INPUT_GET, 'filterProd', FILTER_SANITIZE_STRING);
                         $sql_filter = "SELECT * FROM tb_produtos WHERE categoria = '$filtro' ORDER BY nome_produto";
                         $sql_query_filter = mysqli_query($con, $sql_filter);
 
-                        $filter = $sql_query_filter->fetch_assoc();
+                        $cont = $sql_query_filter->num_rows;
 
-                        do { ?>
+                        if($cont > 0){
 
-                            <div class="prod-item">
+                            $filter = $sql_query_filter->fetch_assoc();
 
-                                <div><?php $imagem = base64_encode($filter['foto']); echo "<img src='data:image/png;base64,$imagem'/>";?></div>
-                                <div class="prod-text">
-                                    <h2><?= $filter['nome_produto'] ?></h2>
+                            do { ?>
+
+                                <div class="prod-item">
+
+                                    <div><?php $imagem = base64_encode($filter['foto']); echo "<img src='data:image/png;base64,$imagem'/>";?></div>
+                                    <div class="prod-text">
+                                        <h2><?= $filter['nome_produto'] ?></h2>
+                                    </div>
+                                    <div class="prod-info">
+                                        <p class="preco">R$<?= $filter['preco'] ?></p>
+                                        <p class="categ">Categoria: <?= $filter['categoria'] ?></p>
+                                        <button class="remove-prod" onclick="apagarProduto(<?= $filter['id_produto']; ?>)"><i class="fa-solid fa-trash-can"></i> Remover</button>
+                                    </div>
+
                                 </div>
-                                <div class="prod-info">
-                                    <p class="preco">R$<?= $filter['preco'] ?></p>
-                                    <p class="categ">Categoria: <?= $filter['categoria'] ?></p>
-                                    <button class="remove-prod" onclick="apagarProduto(<?= $filter['id_produto']; ?>)"><i class="fa-solid fa-trash-can"></i> Remover</button>
-                                </div>
 
+                            <?php } while($filter = $sql_query_filter->fetch_assoc());
+
+                        } else { ?>
+
+                            <div class="prod-vazio">
+                                <h2>Nenhum produto disponível</h2>
                             </div>
 
-                        <?php } while($filter = $sql_query_filter->fetch_assoc());
+                        <?php }
 
                     } ?>
 
